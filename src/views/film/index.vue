e9cbafbd3eb3e4a9ae546cf865049591c12b9d6e
tianAHHH <1337284290@qq.com>
2019-06-29 11:24:06 +0800

    项目搭建完成

---
+++ src/views/film/index.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="page-film">
+        <h1>我是影院详情页面</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "film"
+    }
+</script>
---
+++ src/views/home/films.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="page-home-films">
+        <h1>电影列表院</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "films"
+    }
+</script>
